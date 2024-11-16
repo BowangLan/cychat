3646242e97169f4a12023eafeea05472ec66227f
BowangLan <60339191+BowangLan@users.noreply.github.com>
2024-11-16 13:58:10 -0800

    chore: fix style issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   darkMode: ['class'],
   content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     fontFamily: {
